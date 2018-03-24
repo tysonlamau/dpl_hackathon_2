b8cd7d265157dd87c91fd4036f6cfdd89311681f
Jandrei Pitti <jp.pitti@yahoo.com>
2018-03-24 10:47:09 -0600

    work

--- db/migrate/20180324160839_create_carts.rb
+++ db/migrate/20180324160839_create_carts.rb
@@ -1,8 +1,6 @@
 class CreateCarts < ActiveRecord::Migration[5.1]
   def change
     create_table :carts do |t|
-      t.User :belongs_to
-
       t.timestamps
     end
   end
--- db/migrate/20180324160854_create_dishes.rb
+++ db/migrate/20180324160854_create_dishes.rb
@@ -1,7 +1,6 @@
 class CreateDishes < ActiveRecord::Migration[5.1]
   def change
     create_table :dishes do |t|
-      t.Cart :belongs_to
       t.string :dish
       t.float :price
       t.string :category
