c1b472cc8a02163a49afd5adfbafaf6a03bcf8a8
Shandelle Lovato <ShandelleLovato@gmail.com>
2018-03-24 10:58:05 -0600

    some html formatting

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -6,8 +6,28 @@ class Home extends Component {
   render() {
     return (
       <div>
+        <h1>Menu</h1>
+        <p>TODO:  ADD menu items from db</p>
         <div>
+          <h1>Category</h1>
+          <ul>
+            <li>first</li>
+            <li>second</li>
+            <li>third</li>
+          </ul>
+          <h1>Second Category</h1>
+          <ul>
+            <li>first</li>
+            <li>second</li>
+            <li>third</li>
+          </ul>
+        </div>
 
+        <h1>About</h1>
+        <p>information</p>
+        <div>
+          <h1>Location</h1>
+          <p>Location and contact info</p>
         </div>
 
       </div>
